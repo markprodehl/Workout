b86ec60eba7c9269fa58eb99939e86c0278cf869
Mark Prodehl <markprodehl@gmail.com>
2020-04-29 22:37:07 -0700

    Added post api/workouts, and get api/workouts/range

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,45 @@
 const router = require("express").Router();
 
 // TODO: import required model/s
+const db = require("../models");
 
 // TODO: and add code to the routes so that the app functions correctly
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
-  // CODE HERE
+ 
+  db.Workout.create(req.body)
+  console.log(req.body)
+  .then((data) => {
+    res.json(data);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
-  // CODE HERE
+//  db.Workout.findByIdAndUpdate(req.params.id, {})
+
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  // CODE HERE
+  
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  // CODE HERE
+  db.Workout.find({}).limit(7).sort(-1)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+            console.log(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 // Delete workout with id matching id in the request body.
