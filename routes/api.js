b69f5ea7640089ec092a42e1e73103e106a65f26
Mark Prodehl <markprodehl@gmail.com>
2020-04-29 23:02:06 -0700

    Added get- Api/workouts/range

--- routes/api.js
+++ routes/api.js
@@ -7,15 +7,13 @@ const db = require("../models");
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
- 
   db.Workout.create(req.body)
-  console.log(req.body)
-  .then((data) => {
-    res.json(data);
-  })
-  .catch((err) => {
-    res.status(400).json(err);
-  });
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 // Respond with workout for id url parameter. This should
@@ -27,13 +25,19 @@ router.put("/api/workouts/:id", (req, res) => {
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  
+  db.Workout.find({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({}).limit(7).sort(-1)
-        .then(dbWorkout => {
+  db.Workout.find({}).limit(7).sort({day: -1})
+        .then((dbWorkout) => {
             res.json(dbWorkout);
             console.log(dbWorkout);
         })
